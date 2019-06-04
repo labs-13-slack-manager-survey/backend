e6758f439c0709b9de27953eacf0024cb367afb3
Ben <cbtsao47@gmail.com>
2019-06-04 12:23:13 -0500

    add comments/sentimentRange to responses table

--- data/migrations/20190429162852_responses_table.js
+++ data/migrations/20190429162852_responses_table.js
@@ -1,33 +1,35 @@
 exports.up = function(knex) {
-	return knex.schema.createTable('responses', tbl => {
-		tbl.increments();
+  return knex.schema.createTable("responses", tbl => {
+    tbl.increments();
 
-		tbl
-			.integer('reportId', 128)
-			.unsigned()
-			.notNullable()
-			.references('id')
-			.inTable('reports')
-			.onDelete('CASCADE')
-			.onUpdate('CASCADE');
+    tbl
+      .integer("reportId", 128)
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("reports")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
 
-		tbl
-			.integer('userId', 128)
-			.unsigned()
-			.notNullable()
-			.references('id')
-			.inTable('users')
-			.onDelete('CASCADE')
-			.onUpdate('CASCADE');
+    tbl
+      .integer("userId", 128)
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
 
-		tbl.text('question').notNullable();
+    tbl.text("question").notNullable();
 
-		tbl.text('answer');
+    tbl.text("answer");
 
-		tbl.datetime('submitted_date', { precision: 2 }).notNullable();
-	});
+    tbl.datetime("submitted_date", { precision: 2 }).notNullable();
+    tbl.text("comments");
+    tbl.integer("sentimentRange");
+  });
 };
 
 exports.down = function(knex) {
-	return knex.schema.dropTableIfExists('responses');
+  return knex.schema.dropTableIfExists("responses");
 };
