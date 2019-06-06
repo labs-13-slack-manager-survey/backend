a39a5a4f8b57f9e399cb1bc752d6d1b870a30005
Ben <cbtsao47@gmail.com>
2019-06-05 19:21:41 -0500

    remove logs and change styling to prettier

--- controllers/email.js
+++ controllers/email.js
@@ -12,11 +12,9 @@ router.post("/", async (req, res) => {
       subject: "Welcome to Slackr",
       text: `Join our Slackr team! Go to https://slackrs-app.netlify.com/login, create a login, and enter your join code (${joinCode}) when prompted.`
     };
-    console.log(email);
     const success = await sgMail.send(msg);
     res.status(200).end();
   } catch (error) {
-    console.log(error);
     res.status(500).json({
       message: "Sorry but something went wrong while sending the emails."
     });
