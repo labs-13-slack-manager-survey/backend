21b8c8e92bdce447109df6431403b1d9917de02a
Taylor <wwwaaavvveeerrr@gmail.com>
2019-06-11 02:01:58 -0400

    added two testing enviorments one for local and one for depoyed.

---
+++ cypress/integration/test/local.spec.js
@@ -0,0 +1,11 @@
+describe('Site Live', function() {
+    it('Site Live', function() {
+    cy.visit('http://localhost:3000/')
+    })
+    })
+
+describe('Login', () => {
+    it('Second Load' ,() =>{
+        cy.visit('http://localhost:3000/')
+    })
+})
