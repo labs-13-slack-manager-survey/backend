4ba032c5e07f522f3cb09275b3d7880c59ab0ab7
Ben <cbtsao47@gmail.com>
2019-06-12 18:42:11 -0500

    add historical rate by report

--- helpers/submissionRates.js
+++ helpers/submissionRates.js
@@ -1,9 +1,21 @@
 const Users = require("../models/Users");
 module.exports = {
-  getHistoricalSubmissionRate
+  getHistoricalSubmissionRate,
+  getHistoricalSubmissionRateByReport
 };
 async function getHistoricalSubmissionRate(teamId) {
   const users = await Users.getPollsStatsByTeamId(teamId);
+  const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
+  const responsesMade = users.map(user => JSON.parse(user.responsesMade));
+  const rate =
+    ([].concat.apply([], responsesMade).reduce((a, c) => a + c) /
+      [].concat.apply([], pollsReceived).reduce((a, c) => a + c) || 1) * 100;
+  const result = Number.parseFloat(rate).toFixed(2);
+  return Number(result);
+}
+
+async function getHistoricalSubmissionRateByReport(teamId, reportId) {
+  const users = await Users.getPollsStatsByTeamId(teamId, reportId);
   const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
   const responsesMade = users.map(user => JSON.parse(user.responsesMade));
   const rate =
