d1dfc17328f55331be1dbe87d63284619d60c8ac
Ben <cbtsao47@gmail.com>
2019-06-12 18:24:18 -0500

    add historical submission rate helper function

---
+++ helpers/submissionRates.js
@@ -0,0 +1,14 @@
+const Users = require("../models/Users");
+module.exports = {
+  getHistoricalSubmissionRate
+};
+async function getHistoricalSubmissionRate(teamId) {
+  const users = await Users.getPollsStatsByTeamId(teamId);
+  const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
+  const responsesMade = users.map(user => JSON.parse(user.responsesMade));
+  const rate =
+    ([].concat.apply([], responsesMade).reduce((a, c) => a + c) /
+      [].concat.apply([], pollsReceived).reduce((a, c) => a + c) || 1) * 100;
+  const result = Number.parseFloat(rate).toFixed(2);
+  return Number(result);
+}
