0bb284a61c2db70e9c656b319a2150b0d023a406
Ben <cbtsao47@gmail.com>
2019-06-12 19:01:54 -0500

    adjust historical submission rate of a team to return the correct number

--- helpers/submissionRates.js
+++ helpers/submissionRates.js
@@ -7,9 +7,9 @@ async function getHistoricalSubmissionRate(teamId) {
   const users = await Users.getPollsStatsByTeamId(teamId);
   const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
   const responsesMade = users.map(user => JSON.parse(user.responsesMade));
-  const rate =
-    ([].concat.apply([], responsesMade).reduce((a, c) => a + c) /
-      [].concat.apply([], pollsReceived).reduce((a, c) => a + c) || 1) * 100;
+  const flattenedRes = [].concat.apply([], responsesMade);
+  const flattenedPolls = [].concat.apply([], pollsReceived);
+  const rate = (flattenedRes.length / flattenedPolls.length || 1) * 100;
   const result = Number.parseFloat(rate).toFixed(2);
   return Number(result);
 }
@@ -18,9 +18,13 @@ async function getHistoricalSubmissionRateByReport(teamId, reportId) {
   const users = await Users.getPollsStatsByTeamId(teamId, reportId);
   const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
   const responsesMade = users.map(user => JSON.parse(user.responsesMade));
+  const flattenedPolls = [].concat.apply([], responsesMade);
+  const flattenedRes = [].concat.apply([], pollsReceived);
+  const filteredPolls = flattenedPolls.filter(poll => poll === reportId);
+  const filteredRes = flattenedRes.filter(res => res === reportId);
   const rate =
-    ([].concat.apply([], responsesMade).reduce((a, c) => a + c) /
-      [].concat.apply([], pollsReceived).reduce((a, c) => a + c) || 1) * 100;
+    (filteredRes.reduce((a, c) => a + c) /
+      filteredPolls.reduce((a, c) => a + c) || 1) * 100;
   const result = Number.parseFloat(rate).toFixed(2);
   return Number(result);
 }
