9c79433191f9d0fe599e605c99a264654a96f3e4
Ben <cbtsao47@gmail.com>
2019-06-13 11:53:35 -0500

    include total responses in the submissionRate endpoint

--- helpers/submissionRates.js
+++ helpers/submissionRates.js
@@ -5,6 +5,7 @@ module.exports = {
 };
 async function getHistoricalSubmissionRate(teamId) {
   const users = await Users.getPollsStatsByTeamId(teamId);
+
   // grab polls and responses  received , array of arrays
   const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
   const responsesMade = users.map(user => JSON.parse(user.responsesMade));
@@ -14,7 +15,10 @@ async function getHistoricalSubmissionRate(teamId) {
   // calculate the rate
   const rate = (flattenedRes.length / (flattenedPolls.length || 1)) * 100;
   const result = Number.parseFloat(rate).toFixed(2);
-  return Number(result);
+  return {
+    totalResponses: flattenedRes.length,
+    historicalSubmissionRate: Number(result)
+  };
 }
 
 async function getHistoricalSubmissionRateByReport(teamId, reportId) {
