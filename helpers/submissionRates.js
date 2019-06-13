9b8e079ac0c3dea64c4fbe50e9c1db440cdcce3f
Ben <cbtsao47@gmail.com>
2019-06-12 19:11:23 -0500

    add comments and fix calculation bug

--- helpers/submissionRates.js
+++ helpers/submissionRates.js
@@ -5,10 +5,14 @@ module.exports = {
 };
 async function getHistoricalSubmissionRate(teamId) {
   const users = await Users.getPollsStatsByTeamId(teamId);
+  // grab polls and responses  received , array of arrays
   const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
   const responsesMade = users.map(user => JSON.parse(user.responsesMade));
+  // flatten the array so we can count it
   const flattenedRes = [].concat.apply([], responsesMade);
   const flattenedPolls = [].concat.apply([], pollsReceived);
+
+  // calculate the rate
   const rate = (flattenedRes.length / flattenedPolls.length || 1) * 100;
   const result = Number.parseFloat(rate).toFixed(2);
   return Number(result);
@@ -16,15 +20,22 @@ async function getHistoricalSubmissionRate(teamId) {
 
 async function getHistoricalSubmissionRateByReport(teamId, reportId) {
   const users = await Users.getPollsStatsByTeamId(teamId, reportId);
+  // grab polls and responses received, array of arrays
   const pollsReceived = users.map(user => JSON.parse(user.pollsReceived));
   const responsesMade = users.map(user => JSON.parse(user.responsesMade));
-  const flattenedPolls = [].concat.apply([], responsesMade);
-  const flattenedRes = [].concat.apply([], pollsReceived);
-  const filteredPolls = flattenedPolls.filter(poll => poll === reportId);
-  const filteredRes = flattenedRes.filter(res => res === reportId);
-  const rate =
-    (filteredRes.reduce((a, c) => a + c) /
-      filteredPolls.reduce((a, c) => a + c) || 1) * 100;
+  // flatten the array so we can count it
+  const flattenedRes = [].concat.apply([], responsesMade);
+  const flattenedPolls = [].concat.apply([], pollsReceived);
+  // filter through the array to find the one matching the report id
+
+  const filteredPolls = flattenedPolls.filter(
+    poll => poll === Number(reportId)
+  );
+  console.log(filteredPolls.length);
+  const filteredRes = flattenedRes.filter(res => res === Number(reportId));
+  console.log(filteredRes.length);
+  const rate = (filteredRes.length / filteredPolls.length || 1) * 100;
+
   const result = Number.parseFloat(rate).toFixed(2);
   return Number(result);
 }
