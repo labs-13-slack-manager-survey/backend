0a8506cf55496d1e7c74c813d9eaf3391239f366
Ben <cbtsao47@gmail.com>
2019-06-26 04:04:37 -0500

    add sendFeedBack route for feedback

--- controllers/email.js
+++ controllers/email.js
@@ -12,7 +12,27 @@ router.post("/", async (req, res) => {
       subject: "Welcome to Slackr",
       text: `Join our Slackr team! Go to https://slackrs-app.netlify.com/login, create a login, and enter your join code (${joinCode}) when prompted.`
     };
-    const success = await sgMail.send(msg);
+    await sgMail.send(msg);
+    res.status(200).end();
+  } catch (error) {
+    res.status(500).json({
+      message: "Sorry but something went wrong while sending the emails."
+    });
+    throw new Error(error);
+  }
+});
+router.post("/sendFeedBack", async (req, res) => {
+  try {
+    const { mail } = req.body;
+    const msg = {
+      to: "lambdalabs13@gmail.com",
+      from: "lambdalabs13@gmail.com",
+      subject: "User Feedback",
+      text: `name:${mail.name} \nemail:${mail.email} \nlikes:${
+        mail.likes
+      } \ndislikes:${mail.dislikes}`
+    };
+    await sgMail.send(msg);
     res.status(200).end();
   } catch (error) {
     res.status(500).json({
